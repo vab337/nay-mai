e9a1d15bb0612b18dd6016078d98722231c1eba3
Van Anh Bui <36029601+vab337@users.noreply.github.com>
2023-10-20 05:27:39 +0000

    update 2010

--- getData/sketch-getData.js
+++ getData/sketch-getData.js
@@ -192,7 +192,7 @@
       console.log(imageUrls[0]);
     
       // Create a new window for printing
-      var printWindow = window.open('', '', 'width=595, height=842');
+      var printWindow = window.open('', '', 'width=595, height=800');
     
       // Write the content and include a link to an external CSS file
       printWindow.document.open();
